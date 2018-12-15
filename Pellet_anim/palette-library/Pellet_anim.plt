d7305c0cf1969eb294b77d7c9ffad5d89320a664
Swearingen, Scott V <swearingen.16@osu.edu>
2018-12-15 12:44:35 -0500

    Starting new repository

---
+++ Pellet_anim/palette-library/Pellet_anim.plt
@@ -0,0 +1,8 @@
+ToonBoomAnimationInc PaletteFile 2
+Solid    Black                      0x0963408178c00d9d   0   0   0 255
+Solid    White                      0x0963408178c00d9f 255 255 255 255
+Solid    Red                        0x0963408178c00da1 255   0   0 255
+Solid    Green                      0x0963408178c00da3   0 255   0 255
+Solid    Blue                       0x0963408178c00da5   0   0 255 255
+Solid    "Vectorized Line"          0x0000000000000003   0   0   0 255
+Solid    Tail                       0x0963408178c01586 118 255 255 255
---
+++ test_008/Assets/Scripts/camera_logic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class camera_logic : NetworkBehaviour {
+
+    public GameObject cam_server;
+    public GameObject cam_client;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isServer)
+        {
+            cam_client.SetActive(false);
+            return;
+        }
+
+        if (!isServer)
+        {
+            cam_server.SetActive(false);
+            return;
+        }
+		
+	}
+}
