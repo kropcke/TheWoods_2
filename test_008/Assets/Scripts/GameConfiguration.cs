1aab3c6e99d9b886311debfec7a0f16a0beeb500
Fede Camara Halac <camarafede@gmail.com>
2019-10-25 12:47:50 -0400

    fixed crash on UnityPD.Init()
    	- UnityPD needs an Audio Mixer Group with an added effect called *UnityPd*
    	- Added `UnityPDStart.cs` to the Game Controller object to initialize and open the .pd patch

--- test_008/Assets/Scripts/GameConfiguration.cs
+++ test_008/Assets/Scripts/GameConfiguration.cs
@@ -11,25 +11,13 @@ public class GameConfiguration : MonoBehaviour {
     public AudioSource audioSource;
     internal bool debugMode;
 
-    public string patchName;
-    private int _pdPatch = -1;
-
-
     // Use this for initialization
     void Start () {
-        UnityPD.Init();
-        _pdPatch = UnityPD.OpenPatch ( patchName );
         audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-    void OnApplicationQuit() {
-        if (_pdPatch >= 0) 
-            UnityPD.ClosePatch( _pdPatch );
-        UnityPD.Deinit();
     }
 }
---
+++ test_008/Assets/Scripts/UnityPDStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityPDStart : MonoBehaviour
+{
+    public string patchName;
+    private int _pdPatch = -1;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Initializing UnityPD");
+
+        UnityPD.Init();
+
+        yield return new WaitForSeconds(2f);
+
+        _pdPatch = UnityPD.OpenPatch(patchName);
+    }
+
+    void OnApplicationQuit()
+    {
+        UnityPD.Deinit();
+    }
+}
