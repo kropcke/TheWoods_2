c07c5ee9f7ec3613067d2c696141bd7bca8a7d2e
Matt Hall <matthew349hall@hotmail.com>
2020-04-01 16:46:06 -0400

    PretyBird animation integrations

---
+++ test_008/Assets/Scripts/AnimatorTestScript2.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class AnimatorTestScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    public string paramter;
+    int current;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log(current);
+            anim.SetInteger(paramter, current);
+            current++;
+        }
+
+
+        // Loop the current state
+        if (current > 2)
+        {
+            current = 0;
+        }
+
+    }
+
+    public void SetState2(string s)
+    {
+        if (s == "flying")
+        {
+            anim.SetInteger(paramter, 0);
+
+        }
+        else if (s == "perched")
+        {
+            anim.SetInteger(paramter, 1);
+        }
+        else if (s == "singing")
+        {
+            anim.SetInteger(paramter, 1);
+        }
+    }
+
+    [PunRPC]
+    public void SetState(string s)
+    {
+        if (s == "flying")
+        {
+            anim.SetInteger(paramter, 2);
+
+        }
+        else if (s == "perched")
+        {
+            anim.SetInteger(paramter, 1);
+        }
+        else if (s == "singing")
+        {
+            anim.SetInteger(paramter, 1);
+        }
+    }
+}
