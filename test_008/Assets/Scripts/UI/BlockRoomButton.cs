b72c0fac39ce5c5c08a65a5ce2d23ed069c39afb
Matt Hall <matthew349hall@hotmail.com>
2021-05-12 12:45:51 -0400

    Eula, Block button

---
+++ test_008/Assets/Scripts/UI/BlockRoomButton.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+  using System.IO;
+
+
+public class BlockRoomButton : Link
+{
+    private const string apiURL = "http://thewoods-blocklist-postgres-production.up.railway.app/block/";
+
+
+    public GameObject errorPanel;
+
+    public GameObject inputGameObject;
+    TMP_InputField input;
+
+    static readonly HttpClient httpClient = new HttpClient();
+
+    void Start()
+    {
+        input = inputGameObject.GetComponent<TMP_InputField>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(visitPage);
+    }
+
+    public override void visitPage()
+    {
+        string roomName = input.text;
+
+        string result = AddToBlocklist(roomName);
+        print(result);
+        nextPanel.SetActive(true);
+        thisPanel.SetActive(false);
+    }
+
+
+
+    string AddToBlocklist(string room)
+    {
+        string url = apiURL + room;
+        print(url);
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+        return reader.ReadToEnd();
+
+        // var task = Task.Run(() => httpClient.PostAsync(url, new StringContent(room)));
+
+        // task.Wait();
+        // print("sent");
+
+        // // response.EnsureSuccessStatusCode();
+        // // string content = await response.Content.ReadAsStringAsync();
+        // // print(content);
+        // // var task = Task.Run()
+
+        // var t = Task.Run(() =>  task.Result.Content.ReadAsStringAsync());
+        // t.Wait();
+        // print("response");
+
+        // return t.Result;
+
+
+        // WWWForm form = new WWWForm();
+        // form.AddField("myField", "myData");
+
+
+        // using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        // {
+        //     www.SetRequestHeader("Cache-Control", "max-age=0, no-cache, no-store");
+        //     www.SetRequestHeader("Pragma", "no-cache");
+        //     print(www.url);
+
+        //     yield return www.SendWebRequest();
+
+        //     if (www.isNetworkError || www.isHttpError)
+        //     {
+        //         Debug.Log(www.error);
+        //         nextPanel.SetActive(true);
+        //         thisPanel.SetActive(false);
+        //     }
+        //     else
+        //     {
+        //         Debug.Log("Form upload complete!");
+        //         nextPanel.SetActive(true);
+        //         thisPanel.SetActive(false);
+        //     }
+        // }
+    }
+
+}
---
+++ test_008/Assets/Scripts/UI/OpenEulaButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenEulaButton : Link
+{
+    public string eulaLink = "http://wizaga.com/thewoods.html";
+    private const string prefsKey = "eula_displayed";
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(visitPage);
+
+
+        // If the EULA has already been read, skip this screen.
+        if (PlayerPrefs.GetString(prefsKey, "false") == "true")
+        {
+            nextPanel.SetActive(true);
+            thisPanel.SetActive(false);
+        }
+        
+    }
+
+    public override void visitPage()
+    {
+        Application.OpenURL(eulaLink);
+        PlayerPrefs.SetString(prefsKey, "true");
+        nextPanel.SetActive(true);
+        thisPanel.SetActive(false);
+    }
+
+}
