1a123e4bfcf44326391b627f0f596d48d68a8b37
Ratna Sruthi Ammannagari <ratnasruthi@gmail.com>
2021-05-26 23:43:20 -0400

    Added Scroll View for EULA

---
+++ test_008/Assets/Scripts/UI/AgreeButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgreeButton : Link
+{
+
+    public GameObject newBackground;
+    void Start() {
+        newBackground.gameObject.SetActive(true);
+        button = GetComponent<Button>();
+        button.onClick.AddListener(visitPage);
+    }
+
+    public override void visitPage() {
+        newBackground.gameObject.SetActive(false);
+        nextPanel.SetActive(true);
+        thisPanel.SetActive(false);
+    }
+
+}
---
+++ test_008/Assets/Scripts/UI/EULATextFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class EULATextFormatter : MonoBehaviour
+{
+    private string eulaLink = "http://wizaga.com/TheWoods_PrivacyPolicy.html";
+    private string eulaText = "";
+    
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        WWW www = new WWW(eulaLink);
+        StartCoroutine(WaitForRequest(www));
+        //string eulaText = File.ReadAllText("Assets/Resources/EULA/eula.html");
+        //gameObject.GetComponent<Text>().text = eulaText;
+
+    }
+
+    // Update is called once per frame
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+    
+        // check for errors
+        if (www.error == null)
+        {
+           
+            eulaText = www.text;
+            Debug.Log("WWW Ok!: " + eulaText);
+            gameObject.GetComponent<Text>().text = eulaText;
+        } else {
+            Debug.Log("WWW Error: "+ www.error);
+        }    
+    }
+}
