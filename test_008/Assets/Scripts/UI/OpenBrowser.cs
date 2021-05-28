2272a930350ff8f62ff2636a4cf1795975551767
Matt Hall <matthew349hall@hotmail.com>
2021-05-28 13:38:07 -0400

    about page

---
+++ test_008/Assets/Scripts/UI/OpenBrowser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenBrowser : Link
+{
+    public string eulaLink = "http://wizaga.com/thewoods.html";
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(visitPage);
+
+
+        
+    }
+
+    public override void visitPage()
+    {
+        Application.OpenURL(eulaLink);
+        // nextPanel.SetActive(true);
+        // thisPanel.SetActive(false);
+    }
+}
